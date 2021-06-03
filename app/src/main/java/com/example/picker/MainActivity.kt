0fe4a4b77ac644eb7dd09b62702f961f83bb4304
evgenkostrov <evgenkostrov@gmail.com>
2021-06-03 20:35:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/picker/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.picker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.textView)
+        val number = findViewById<NumberPicker>(R.id.number)
+        val names = resources.getStringArray(R.array.man_names)
+        number.minValue=0
+        number.maxValue=4
+        number.displayedValues = names
+        number.setOnValueChangedListener { picker, oldVal, newVal ->  text.text="I will always love you ${names[newVal]}" }
+
+
+
+        }
+    }
+
